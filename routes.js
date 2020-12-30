3dd6f121a10e454bac06d837aa80a989f73cca8a
Dhakshin <dhakshin01@gmail.com>
2020-12-29 18:24:49 -0800

    added put and delete route

--- routes.js
+++ routes.js
@@ -1,48 +1,97 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const mongoose = require('mongoose');
-const Store = require('./models/store_model')
+const mongoose = require("mongoose");
+const Store = require("./models/store_model");
 
+router.get("/", (req, res) => {
+  Store.find({}, (err, stores) => {
+    if (err) {
+      res.status(404).send(err);
+    } else {
+      res.status(200).send(stores);
+    }
+  });
+});
 
-router.get('/', (req, res) => {
-    Store.find({}, (err, stores) => {
-        if(err){
-            console.log(err)
-            
-        }else{
-            res.status(200).send(stores);
+router.post("/", (req, res) => {
+  let new_id = new mongoose.Types.ObjectId();
+  const NewStore = new Store({
+    _id: new_id,
+    name: req.body.name,
+    zip: parseInt(req.body.zip),
+    city: req.body.city,
+    cost: req.body.cost,
+    link: req.body.link,
+    desc: req.body.desc,
+  });
+  NewStore.save((err, doc) => {
+    if (err) {
+      return res.status(400).send("Cannot save store", err);
+    } else {
+      res.status(200).send(`New store with id: ${new_id} created`);
+    }
+  });
+});
+
+router.put("/:id", (req, res) => {
+  var store_id = req.params.id;
+  let update = {
+    name: req.body.name,
+    zip: req.body.zip,
+    city: req.body.city,
+    cost: req.body.cost,
+    link: req.body.link,
+    desc: req.body.desc,
+  };
+
+  let changes = !Object.values(update).every((m) => m === null);
+
+  Store.findById(store_id, (err, found_store) => {
+    if (!found_store) {
+      return res.status(404).send("Store not found");
+    } else if (changes) {
+      for (let [key, value] of Object.entries(update)) {
+        if (typeof value !== "undefined") {
+          console.log(`${key}: ${value}`);
+          if (key == "name") {
+            found_store.name = req.body.name;
+          } else if (key == "zip") {
+            found_store.zip = parseInt(req.body.zip);
+          } else if (key == "city") {
+            found_store.city = req.body.cost;
+          } else if (key == "cost") {
+            found_store.cost = req.body.cost;
+          } else if (key == "link") {
+            found_store.link = req.body.link;
+          } else if (key == "desc") {
+            found_store.desc = req.body.desc;
+          }
         }
-    });
-});
+      }
 
-
-router.post('/', (req, res) => {
-    const store = new Store({
-        _id: new mongoose.Types.ObjectId(),
-        name: req.body.name,
-        zip: parseInt(req.body.zip),
-        city: req.body.city,
-        cost: req.body.cost,
-        link: req.body.link,
-        desc: req.body.desc
-    });
-    store.save((err,doc)=>{
-        if(err){
-            res.status(400).send("Cannot save store")
-            return console.log(err)
-        }else{
-            res.status(200).send("done");
+      found_store.save((err, updated_store) => {
+        if (err) {
+          return res.status(400).send(err);
+        } else {
+          return res.status(200).send(updated_store);
         }
-    });
-    
+      });
+    } else {
+      return res.status(200).send(`No changes made to store of id:${store_id}`);
+    }
+  });
 });
 
-router.put('/', (req, res) => {
+router.delete("/:id", (req, res) => {
+  var store_id = req.params.id;
 
-});
-
-router.delete('/:id', (res, req) => {
-
+  Store.deleteOne({ _id: store_id }, (err) => {
+    if (err) {
+      res.status(500).send(`Unable to delete store of id: ${store_id}`, err);
+    } else {
+      res.status(200).send(`Deleted store with id: ${store_id}`);
+    }
+  });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,20 +1,35 @@
-const mongoose = require('mongoose');
-const express = require('express');
-const { MONGO_USER, MONGO_PASSWORD, DBNAME, PORT } = require('./config')
+const mongoose = require("mongoose");
+const express = require("express");
+const { MONGO_USER, MONGO_PASSWORD, DBNAME, PORT } = require("./config");
 const app = express();
 app.use(express.json());
 
-console.log(MONGO_USER, MONGO_PASSWORD, DBNAME, PORT)
-mongoose.connect('mongodb+srv://' + MONGO_USER + ':' + MONGO_PASSWORD + '@cluster0.qgggw.mongodb.net/' + DBNAME + '?retryWrites=true&w=majority', {
-    useMongoClient: true
-})
+mongoose.connect(
+  "mongodb+srv://" +
+    MONGO_USER +
+    ":" +
+    MONGO_PASSWORD +
+    "@cluster0.qgggw.mongodb.net/" +
+    DBNAME +
+    "?retryWrites=true&w=majority",
+  {
+    useMongoClient: true,
+  }
+);
+app.use("/api/stores", require("./routes"));
+app.get("/", (req, res) => {
+  res.send("FitNut mainpage");
+});
+app.get("/*", (req, res) => {
+  res.status(404).send("404: Page not found");
+});
 
-app.use('/api/stores', require('./routes'));
-app.get('/', (req, res) => {
-    res.send("FitNut mainpage")
-})
-app.get('/*', (req, res) => {
-    res.status(404).send("404: Page not found")
-})
-app.listen(PORT, () => console.log(`server started on ${PORT}`));
+var server = app.listen(parseInt(PORT), () =>
+  console.log(`server started on ${PORT}`)
+);
 
+process.on("exit" || "uncaughtException" || "SIGTERM", () => {
+  server.close(() => {
+    console.log("Process terminated");
+  });
+});
